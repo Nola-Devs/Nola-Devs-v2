c0c0f5ce9f0fa44b44ae0a749f82e34edd202df7
codingMustache <contact@codingMustache.dev>
2023-11-27 21:24:35 -0600

    basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+	import { GroupNames } from '$data';
+		
+</script>
+
+{#each GroupNames as group}
+	<a href="/group={group}">
+		<p>
+			{group}
+		</p>
+	</a>
+{/each}
 <slot />
