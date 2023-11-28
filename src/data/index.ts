5e580a8cfc6beba059ba82ffb1f3af2639cc1b4b
codingMustache <contact@codingMustache.dev>
2023-11-27 21:23:36 -0600

    common Utilities made

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,15 @@
 // place files you want to import through the `$data` alias in this folder.
+import type { Group } from '../app'
+import GroupsData from './groups.json';
 
-export const hello = () => {};
+const GroupNames: string[] = GroupsData
+  .map((group: any) => group.group)
+  .filter(Boolean);
+
+const findGroupByName = (targetGroup: string): Group => {
+  const groups = GroupsData as unknown as Group[]
+  return groups.find((group: Group) => group.group === targetGroup) || GroupsData[0] as unknown as Group;
+};
+
+
+export {Group, GroupNames, findGroupByName}
