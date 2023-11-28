f0c6c9614512bcea8dbec24ef14c8a02a0985e0e
codingMustache <contact@codingMustache.dev>
2023-11-27 21:20:23 -0600

    Added types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,6 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -9,4 +10,36 @@ declare global {
 	}
 }
 
-export {};
+interface Event {
+  summary: string;
+  calLink: string;
+  description: string;
+  location: string;
+  start: Date;
+  end: Date;
+}
+
+interface PageServerLoadResult {
+  groupObj: Group;
+  events: Event[];
+}
+type linkKey = "linkedin" | "facebook" | "twitter" | "github" | "twitch" | "eventbrite" | "meetup" | "youtube" | "linktr" | "email" | "website" | string
+
+type Link ={[key: linkKey]: string}
+
+type Organizers = {
+  name:string,
+  pfp:string,
+  links: Link
+}
+
+interface Group {
+	group: string,
+	about: string,
+	calID: string,
+	orgLinks: Link,
+	organizers: Organizers[]
+}
+
+
+export { Event, Group, Organizers, PageServerLoadResult };
