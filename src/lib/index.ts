aa2bcd0bb034fba2b266fb241ad12dfe2fd85fee
codingMustache <contact@codingMustache.dev>
2023-11-27 21:24:10 -0600

    html sanitizer added a

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 // imports
-import { Sanitizer } from "./sanitize";
+import { Sanitizer } from './sanitize';
 
 // exports
-export { Sanitizer }
-
+export { Sanitizer };
--- src/lib/sanitize.ts
+++ src/lib/sanitize.ts
@@ -1,14 +1,11 @@
 import sanitizeHtml from 'sanitize-html';
 
 export const Sanitizer = (dirty: string) => {
-  const clean = sanitizeHtml(dirty,
-    {
-      allowedTags: ['a', 'p'],
-      allowedAttributes: {
-        'a': ['href']
-      },
-    }
-  )
-  return clean
-}
-
+	const clean = sanitizeHtml(dirty, {
+		allowedTags: ['a', 'p'],
+		allowedAttributes: {
+			a: ['href']
+		}
+	});
+	return clean;
+};
