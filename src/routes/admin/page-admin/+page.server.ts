8179a48e17bf1708f92e17c1fac7df231c3a6c7a
codingMustache <contact@codingMustache.dev>
2023-12-29 12:22:47 -0600

    formatting

--- src/routes/admin/page-admin/+page.server.ts
+++ src/routes/admin/page-admin/+page.server.ts
@@ -1,18 +1,17 @@
 import type { Actions, PageServerLoad } from './$types';
 import { ADMIN_PW } from '$env/static/private';
 
-
 export const load: PageServerLoad = async ({ params }) => {
-  return { test: 'test' };
+	return { test: 'test' };
 };
 
 export const actions: Actions = {
-  login: async ({ request }) => {
-    const pw = await (await request.formData()).get('password');
-    if (pw === ADMIN_PW) {
-      return { success: true };
-    } else {
-      return { success: false };
-    }
-  }
+	login: async ({ request }) => {
+		const pw = await (await request.formData()).get('password');
+		if (pw === ADMIN_PW) {
+			return { success: true };
+		} else {
+			return { success: false };
+		}
+	}
 };
