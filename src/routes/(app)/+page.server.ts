3f570f8f0350e70a05004b287a7773b94dce3ec3
codingMustache <contact@codingMustache.dev>
2024-01-08 18:59:07 -0600

    Title: Fix Events on Home Page Details: Fixed Where Events are coming from. Now its coming from the   events table and not the Groups Table

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,11 +1,15 @@
 import type { PageServerLoad } from './$types';
 import { GroupModel } from '$lib/db/groups';
+import EventModel from '$lib/db/events';
 import type { Event } from '$types';
 
 export const load: PageServerLoad = async () => {
-	const events = await GroupModel.distinct('events');
-	const removeHN = events
-		.filter((event) => event.summary !== 'Hack Night ')
+	//const events = await GroupModel.distinct('events');
+	const events = (await EventModel.find({})
+		.select(['-_id', '-__v'])
+		.lean()) as Event[]
+
+	const removeHN: Event[] = events.filter((event: any) => event.summary !== 'Hack Night ')
 		.sort(
 			(a: Event, b: Event) => new Date(a.start.date).getTime() - new Date(b.start.date).getTime()
 		) as Event[];
