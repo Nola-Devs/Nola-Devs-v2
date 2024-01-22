792f42dfc9728b1b27ed95500e73c037a15d2502
codingMustache <contact@codingMustache.dev>
2024-01-21 18:59:01 -0600

    Form for editing Users

--- src/routes/admin/organizer/+page.svelte
+++ src/routes/admin/organizer/+page.svelte
@@ -1,13 +1,49 @@
 <script lang="ts">
-	import { Input, Label } from 'flowbite-svelte';
+	import { Tabs, TabItem, Input, Label, Fileupload, Helper, Button } from 'flowbite-svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
-	console.log(data);
+	$: user = data.user;
+	$: group = data.userGroup;
 </script>
 
-{JSON.stringify(data)}
+{#if user?.name}
+	Welcome {user.name}
+{/if}
 
-<form action="">
-	<Label for="first_name" class="mb-2">First name</Label>
-	<Input type="text" id="first_name" placeholder="John" required />
-</form>
+<Tabs>
+	<TabItem open title="Organizer">
+		<form action="?/editUser" method="post" class="flex flex-col p-5">
+			<Label for="name" class="mb-2">Name</Label>
+			<Input type="text" name="name" placeholder="{user?.name}" />
+
+			<Label for="name" class="mb-2">Group</Label>
+			<Input type="text" name="group" placeholder="{user?.group}" />
+
+			<Label for="with_helper" class="pb-2">Upload file</Label>
+			<Fileupload name="pfp" class="mb-2" />
+			<Helper>SVG, PNG, JPG or GIF (MAX. 800x400px).</Helper>
+
+			<Label for="links" class="mb-2">Organizer's Links</Label>
+			<Input type="text" id="links" placeholder="{user?.links}" />
+
+			<Button class="w-fit m-3" type="submit">Save</Button>
+		</form>
+	</TabItem>
+	<TabItem title="Group">
+		<form action="?/editOrg">
+			<Label for="groupName">Group Name</Label>
+			<Input type="text" id="groupName" placeholder="{group?.group}" required />
+
+			<Label for="about">About</Label>
+			<Input type="text" id="about" placeholder="{group?.about}" required />
+
+			<Label for="groupName">Google Calendar ID</Label>
+			<Input type="text" id="groupName" placeholder="{group?.name}" required />
+
+			<Label for="groupName">Organization's Links</Label>
+			<Input type="text" id="groupName" placeholder="{user?.links}" required />
+
+			<Button class="w-fit m-3" type="submit">Save</Button>
+		</form>
+	</TabItem>
+</Tabs>
