5fb62983c82b2de031a17a148984bb16055ce034
codingMustache <contact@codingMustache.dev>
2024-01-21 18:58:50 -0600

    Added Form Actions to From

--- src/routes/admin/organizer/+page.server.ts
+++ src/routes/admin/organizer/+page.server.ts
@@ -1,7 +1,8 @@
 import { SessionModel } from '$lib/db/sessions';
 import { UserModel } from '$lib/db/users';
 import { GroupModel } from '$lib/db/groups.js';
-import { redirect } from '@sveltejs/kit';
+
+import { redirect, type Actions } from '@sveltejs/kit';
 
 
 export const load = async ({ cookies }) => {
@@ -19,23 +20,41 @@ export const load = async ({ cookies }) => {
         'links',
         'email',
         'group'
-      ]))?.toObject();
+      ]))?.toObject()
 
-    const userGroup = await GroupModel
-      .find({ group: user?.group })
+    const userGroup = (await GroupModel
+      .findOne({ group: user?.group })
       .select([
         '-_id',
         'group',
         'about',
         'calID',
         'orgLinks',
-      ])
-
+      ]))?.toObject()
 
     return {
       user,
       userGroup
     }
   }
+}
 
+
+export const actions: Actions = {
+  logout: async ({ cookies }) => {
+    cookies.delete('session');
+    throw redirect(302, '/');
+    return { success: true };
+  },
+  editUser: async ({ request, cookies }) => {
+    const formData = (await request.formData()).entries()
+    const userID = await SessionModel.findOne({ id: cookies.get('session') }).select(['-_id', 'user'])
+
+    for (const pair of formData) {
+      if (pair[1]) {
+        await UserModel.findByIdAndUpdate(userID?.user, { [pair[0]]: pair[1] })
+      }
+    }
+
+  }
 }
