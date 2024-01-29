4579216273c4a5bdc4f55924c685f9f0999d46b2
codingMustache <contact@codingMustache.dev>
2024-01-29 08:15:33 -0600

    Fixing up admin/organizer

--- src/routes/admin/organizer/+page.server.ts
+++ src/routes/admin/organizer/+page.server.ts
@@ -1,5 +1,5 @@
 import { SessionModel } from '$lib/db/sessions';
-import { UserModel } from '$lib/db/users';
+import UserModel from '$lib/db/users';
 import { GroupModel } from '$lib/db/groups.js';
 
 import { redirect, type Actions } from '@sveltejs/kit';
@@ -7,34 +7,24 @@ import { redirect, type Actions } from '@sveltejs/kit';
 export const load = async ({ cookies }) => {
 	// Auth
 	const browserSes = cookies.get('session');
-	const dbSes = await SessionModel.findOne({ id: browserSes }).select(['expire', 'user', 'id']);
+	const dbSes = await SessionModel.findOne({ id: browserSes })
+		.select(['expire', 'user', 'id']);
 
 	if (dbSes?.id === browserSes && dbSes) {
-		const user = (
-			await UserModel.findById(dbSes?.user).select([
-				'-_id',
-				'name',
-				'pfp',
-				'links',
-				'email',
-				'group'
-			])
-		)?.toObject();
+		const user = (await UserModel.findById(dbSes?.user)
+			.select(['-_id', 'name', 'pfp', 'links', 'email', 'group']))?.toObject();
 
-		const userGroup = (
-			await GroupModel.findOne({ group: user?.group }).select([
-				'-_id',
-				'group',
-				'about',
-				'calID',
-				'orgLinks'
-			])
-		)?.toObject();
+		const userGroup = (await GroupModel.findOne({ group: user?.group })
+			.select(['-_id', 'group', 'about', 'calID', 'orgLinks']))?.toObject();
 
-		return {
-			user,
-			userGroup
-		};
+		if (!userGroup) {
+			const groups = await GroupModel.distinct('group')
+			return { user, userGroup, groups };
+		} else {
+			return { user, userGroup }
+		}
+	} else {
+		throw redirect(302, '/');
 	}
 };
 
@@ -42,19 +32,36 @@ export const actions: Actions = {
 	logout: async ({ cookies }) => {
 		cookies.delete('session');
 		throw redirect(302, '/');
-		return { success: true };
+	},
+	resetPassword: async ({ request, cookies }) => {
+		cookies.get('sessionId')
 	},
 	editUser: async ({ request, cookies }) => {
 		const formData = (await request.formData()).entries();
-		const userID = await SessionModel.findOne({ id: cookies.get('session') }).select([
-			'-_id',
-			'user'
-		]);
-
+		const links = {}
 		for (const pair of formData) {
-			if (pair[1]) {
-				await UserModel.findByIdAndUpdate(userID?.user, { [pair[0]]: pair[1] });
+			if (pair[0].match(/link/)) {
+				let host = new URL(String(pair[1])).hostname
+				console.log(pair[0], pair[1], host);
+				// parse links for
+				/**
+				 * Linkedin
+				 * Github
+				 * Twitter/X
+				 * Website
+				 */
 			}
 		}
+
+
+		// const userID = await SessionModel
+		// 	.findOne({ id: cookies.get('session') })
+		// 	.select(['-_id', 'user']);
+
+		// for (const pair of formData) {
+		// 	if (pair[1]) {
+		// 		await UserModel.findByIdAndUpdate(userID?.user, { [pair[0]]: pair[1] });
+		// 	}
+		// }
 	}
 };
--- src/routes/admin/organizer/+page.svelte
+++ src/routes/admin/organizer/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { Tabs, TabItem, Input, Label, Fileupload, Helper, Button } from 'flowbite-svelte';
+	import EditUser from '$lib/components/edit-user.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 	$: user = data.user;
 	$: group = data.userGroup;
+	const groups = data.groups?.map((g) => {
+		return { value: g, name: g };
+	});
 </script>
 
 {#if user?.name}
@@ -11,23 +15,8 @@
 {/if}
 
 <Tabs>
-	<TabItem open title="Organizer">
-		<form action="?/editUser" method="post" class="flex flex-col p-5">
-			<Label for="name" class="mb-2">Name</Label>
-			<Input type="text" name="name" placeholder="{user?.name}" />
-
-			<Label for="name" class="mb-2">Group</Label>
-			<Input type="text" name="group" placeholder="{user?.group}" />
-
-			<Label for="with_helper" class="pb-2">Upload file</Label>
-			<Fileupload name="pfp" class="mb-2" />
-			<Helper>SVG, PNG, JPG or GIF (MAX. 800x400px).</Helper>
-
-			<Label for="links" class="mb-2">Organizer's Links</Label>
-			<Input type="text" id="links" placeholder="{user?.links}" />
-
-			<Button class="w-fit m-3" type="submit">Save</Button>
-		</form>
+	<TabItem open="{!!groups?.length}" title="Organizer">
+		<EditUser />
 	</TabItem>
 	<TabItem title="Group">
 		<form action="?/editOrg">
@@ -38,7 +27,7 @@
 			<Input type="text" id="about" placeholder="{group?.about}" required />
 
 			<Label for="groupName">Google Calendar ID</Label>
-			<Input type="text" id="groupName" placeholder="{group?.name}" required />
+			<Input type="text" id="groupName" placeholder="{group?.calID}" required />
 
 			<Label for="groupName">Organization's Links</Label>
 			<Input type="text" id="groupName" placeholder="{user?.links}" required />
