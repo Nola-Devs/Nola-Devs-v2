829ded7aa882701f66c4c6cc1d69ca6450e5634d
codingMustache <contact@codingMustache.dev>
2024-01-29 08:12:01 -0600

    Fixed Admin navbar

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '/src/app.css';
+	import { DarkMode } from 'flowbite-svelte';
+	import LOGO from '$lib/assets/icons/Logo-wide.svelte';
 </script>
+
+<div class="flex justify-between">
+	<div class="h-8 w-24 m-4">
+		<a href="/">
+			<LOGO />
+		</a>
+	</div>
+	<div class="h-fit my-auto">
+		<DarkMode />
+	</div>
+</div>
 
 <slot />
