de83f023a3af8ed7a8166fc6ef48fdf95f799f80
codingMustache <contact@codingMustache.dev>
2024-01-29 08:11:19 -0600

    Formatting

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -3,5 +3,6 @@ import { MONGO_URL, DB_NAME } from '$env/static/private';
 
 // setting up the connection to the DB
 export const start_db = async () => {
-	return await connect(MONGO_URL + DB_NAME).then(() => console.log('connected'));
+	return await connect(MONGO_URL + DB_NAME)
+		.then(() => console.log('connected'));
 };
