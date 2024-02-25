570398f796e4af9fa45dd921dac534cf481495e9
Jorge <jorge@codingmustache.dev>
2024-02-25 01:39:49 -0600

    backend for leader board information

---
+++ src/routes/(app)/contributors/+page.server.ts
@@ -0,0 +1,15 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+	const request = await fetch(`https://api.github.com/repos/Nola-Devs/Nola-Devs-v2/contributors`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+
+	// print data from the fetch on screen
+	const contributorsList: any = await request.json();
+	console.log(contributorsList);
+	return { data: contributorsList };
+};
