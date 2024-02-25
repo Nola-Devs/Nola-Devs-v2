5bbf916fe3f930b2a576ad73cb3ea94f750ac5b9
Jorge <jorge@codingmustache.dev>
2024-02-25 01:37:26 -0600

    navbar compo to handle full and mobile menus

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -2,10 +2,11 @@
 	import Sidebar from '$lib/components/sidebar.svelte';
 	import MobileMenu from '$lib/components/mobile.svelte';
 	import LogoWide from '$lib/assets/icons/Logo-wide.svelte';
-	import { Card } from 'flowbite-svelte';
 	export let groups: string[];
+	let y: number;
 </script>
 
+<svelte:window bind:innerWidth="{y}" />
 <!--
 	TODO: Implement a seach Feature
 	<div>
@@ -14,10 +15,11 @@
 		</Input>
 	</div>
 	-->
-<a href="/">
-	<LogoWide styles="w-34 h-12 m-0 top-1 left-1 fixed " />
+<a href="/" class="fixed top-1 left-1 mt-3 z-10 bg-white rounded-xl dark:bg-slate-800">
+	<LogoWide styles="w-34 h-12 m-0 p-1" />
 </a>
-
-<Sidebar {groups} />
-
-<MobileMenu {groups} />
+{#if y > 640}
+	<Sidebar {groups} />
+{:else}
+	<MobileMenu {groups} />
+{/if}
