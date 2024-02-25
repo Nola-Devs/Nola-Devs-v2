1b7c0227d63f756d01de02d374d4c9bca02f7f50
Jorge <jorge@codingmustache.dev>
2024-02-25 01:40:18 -0600

    front end for leaderboard info

---
+++ src/routes/(app)/contributors/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { P, Avatar, Heading, A, Card } from 'flowbite-svelte';
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<div class="flex flex-col gap-2 w-96 mx-auto">
+	<Heading class="text-center">Leaderboard</Heading>
+	<div class="grid grid-cols-[200px_50px]">
+		<P>Contributor</P>
+		<P>Contributions</P>
+	</div>
+
+	{#each data.data as person}
+		<Card>
+			<div class="grid grid-cols-[50px_150px_50px] items-center">
+				<Avatar src="{person.avatar_url}" />
+				<P>
+					{person.login}
+				</P>
+				<P class="justify-self-end">
+					{person.contributions}
+				</P>
+			</div>
+		</Card>
+	{/each}
+</div>
