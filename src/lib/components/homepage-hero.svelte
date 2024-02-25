1e056448a6297cf8960e82fbab5ffc32d95b86ec
Jorge <jorge@codingmustache.dev>
2024-02-25 01:35:24 -0600

    hero for the home page

--- src/lib/components/homepage-hero.svelte
+++ src/lib/components/homepage-hero.svelte
@@ -3,27 +3,25 @@
 	import { Slack, Github, Discord } from 'svelte-simples';
 </script>
 
-<div class="border p-3 rounded-lg flex">
-	<div class="w-3/4 m-4 self-end">
-		<Heading class="float-end">We find tech events and bring them to you.</Heading>
-		<P class="my-3"
-			>All you need to bring is yourself! Explore a growing community of curious people wanting to
-			share ideas and learn from others.</P
-		>
-		<div class="flex gap-3">
-			<Button class="flex gap-2 w-32">
-				<Slack class="fill-inherit dark:fill-white" />
-				<P>Slack</P>
-			</Button>
+<div class="p-5 w-full">
+	<Heading class="float-end">We find tech events and bring them to you.</Heading>
+	<P class="my-3 text-wrap font-thin"
+		>All you need to bring is yourself! Explore a growing community of curious people wanting to
+		share ideas and learn from others.
+	</P>
+	<div class="flex gap-3 w-full justify-center">
+		<Button class="flex gap-2 w-32">
+			<Slack class="fill-inherit dark:fill-white" />
+			<P>Slack</P>
+		</Button>
 
-			<Button class="flex gap-2 w-32">
-				<Discord class="fill-inherit dark:fill-white" />
-				<P>Discord</P>
-			</Button>
-			<Button class="flex gap-2 w-32">
-				<Github class="fill-inherit dark:fill-white" />
-				<P>Github</P>
-			</Button>
-		</div>
+		<Button class="flex gap-2 w-32">
+			<Discord class="fill-inherit dark:fill-white" />
+			<P>Discord</P>
+		</Button>
+		<Button class="flex gap-2 w-32">
+			<Github class="fill-inherit dark:fill-white" />
+			<P>Github</P>
+		</Button>
 	</div>
 </div>
