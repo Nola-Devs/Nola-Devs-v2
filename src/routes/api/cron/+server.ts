8bc7aab4a688d90eab4229a3272ccd7333cfbae5
Jorge <jorge@codingMustache.dev>
2024-03-04 18:21:59 -0600

    maybe a fix

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -4,16 +4,16 @@ import { GroupModel } from '$lib/db/groups';
 import { eventParser } from '$lib/utils/event-parser.js';
 import { googleCalAPICall } from '$lib/utils/google-cal-api-cal.js';
 import { revGeocode } from '$lib/utils/rev-geocode.js';
+import {start_db}from '$lib/db/db'
 import type { Event, Group } from '$types';
-import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ request }) => {
 	// auth
-	// const authHeader = request.headers.get('authorization');
-	// if (authHeader !== `Bearer ${CRON_SECRET}`) {
-	// 	return new Response('You Shall Not Pass!', { status: 401 });
-	// }
+	const authHeader = request.headers.get('authorization');
+	if (authHeader !== `Bearer ${CRON_SECRET}`) {
+		return new Response('You Shall Not Pass!', { status: 401 });
+	}
 
 	// TODO: add error handling
 	// TODO: Check for expired sessions
@@ -22,43 +22,38 @@ export const GET: RequestHandler = async ({ request }) => {
 	//   This is the default calendar for the one off turn this into a pvt key
 	//  545d217a064ce6f846e820045fccdaae17db65ee1b53dc14ea249833b94d0f70@group.calendar.google.com
 	// */
-
+	await start_db();
 	const calList: Group[] = await GroupModel.find({}).select(['group', 'calID']);
 	// TODO: Change all Promise.all into Promise.allSettled for better error handling
-	try {
 
-		const events = (
-			await Promise.all(
-				Object.values(calList)
-					.map((e) => e.calID)
-					.map(googleCalAPICall)
-			)
+	const events = (
+		await Promise.all(
+			Object.values(calList)
+				.map((e) => e.calID)
+				.map(googleCalAPICall)
 		)
-			.map((eventsObj, i) =>
-				eventsObj.items.map((event: any): any => {
-					return { ...event, group: calList[i].group };
-				})
-			)
-			.flat();
+	)
+		.map((eventsObj, i) =>
+			eventsObj.items.map((event: any): any => {
+				return { ...event, group: calList[i].group };
+			})
+		)
+		.flat();
 
-		const eventsWithLatLon = (
-			await Promise.all(
-				Object.values(events)
-					.map((e) => e.location)
-					.map(revGeocode)
-			)
-		).map((e, i) => ({ ...events[i], latLon: e }));
+	const eventsWithLatLon = (
+		await Promise.all(
+			Object.values(events)
+				.map((e) => e.location)
+				.map(revGeocode)
+		)
+	).map((e, i) => ({ ...events[i], latLon: e }));
 
-		const parseEvents: Event[] = eventsWithLatLon.map((e) => eventParser(e));
+	const parseEvents: Event[] = eventsWithLatLon.map((e) => eventParser(e));
 
-		console.log(parseEvents);
-		EventModel.collection.drop();
+	console.log(parseEvents);
+	EventModel.collection.drop();
 
-		EventModel.bulkSave(parseEvents.map((e) => new EventModel(e)));
+	EventModel.bulkSave(parseEvents.map((e) => new EventModel(e)));
 
-		return new Response(JSON.stringify(parseEvents), { status: 200 });
-	} catch (error) {
-		console.log(error)
-		throw error;
-	}
+	return new Response(JSON.stringify(parseEvents), { status: 200 });
 };
