aafa001b22f6f7d6d9bf2f76eae22ee09d9c58e4
adonis <lacurly00@gmail.com>
2024-03-14 01:57:44 -0500

    Add components and update layout in +page.svelte

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
-	import HomePageHero from '$lib/components/homepage-hero.svelte';
 	import type { Event } from '$types';
 	import type { PageData } from './$types';
-	import EventList from '$lib/components/event-list.svelte';
 
 	export let data: PageData;
 	let events: Event[] = data.events;
+
+	import CustomAlert, { AlertType } from '$lib/components/alert.svelte';
+	import Nav from '$lib/components/nav.svelte';
+	import Slider from '$lib/components/slider.svelte';
+	import Breadcrumb from '$lib/components/breadcrumb.svelte';
+	import EventList from '$lib/components/event-list.svelte';
 </script>
 
-<div class="grid grid-rows-[1fr_auto] items-center h-full">
-	<HomePageHero />
-
+<div class="w-full flex flex-col gap-6 h-full">
+	<!-- Alert -->
+	<CustomAlert
+		type="{AlertType.Update}"
+		message="We've updated our site, let us know how we're doing by filling a short survey"
+		link="/survey"
+		linkText="here"
+	/>
+	<!-- Nav -->
+	<Nav />
+	<!-- BreadCrumb -->
+	<Breadcrumb />
+	<!-- Banner Slider -->
+	<Slider />
+	<!-- Upcomming events -->
 	<EventList {events} />
 </div>
