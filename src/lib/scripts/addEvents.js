fb19a30c17c7649a86ec0efde6173af0984090f3
adonis <lacurly00@gmail.com>
2024-03-13 19:25:32 -0500

    fix(scripts): update data seeding scripts to use centralized db connection and models

--- src/lib/scripts/addEvents.js
+++ src/lib/scripts/addEvents.js
@@ -1,26 +1,7 @@
 import events from './data/events.json' assert { type: 'json' };
-import { model, Schema } from 'mongoose';
-
-const eventSchema = new Schema({
-	group: String,
-	summary: String,
-	calLink: String,
-	description: String,
-	location: String,
-	lnglat: [Number, Number],
-	start: {
-		date: String,
-		time: String
-	},
-	end: {
-		date: String,
-		time: String
-	}
-});
-
-const EventModel = model('Event', eventSchema);
+import EventModel from '$lib/db/events';
 
 export const loadEvents = async () => {
-	const res = await EventModel.bulkSave(events.map((e) => new EventModel(e)));
-	console.log(res);
+    const res = await EventModel.bulkSave(events.map((e) => new EventModel(e)));
+    console.log(res);
 };
--- src/lib/scripts/addGroups.js
+++ src/lib/scripts/addGroups.js
@@ -1,26 +1,5 @@
 import groups from './data/groups.json' assert { type: 'json' };
-import { model, Schema } from 'mongoose';
-
-const groupSchema = new Schema({
-	group: {
-		type: String,
-		required: false
-	},
-	about: {
-		type: String,
-		required: true
-	},
-	calID: {
-		type: String,
-		required: true
-	},
-	links: {
-		type: Object,
-		required: false
-	}
-});
-
-const GroupModel = model('Group', groupSchema);
+import GroupModel from '$lib/db/groups';
 
 export const loadGroups = async () => {
 	const res = await GroupModel.bulkSave(groups.map((e) => new GroupModel(e)));
