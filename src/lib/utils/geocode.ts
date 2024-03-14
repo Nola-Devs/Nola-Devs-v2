6c09467e95f89023dcde36c55e7eb525cb790cb1
Jorge <jorge@codingMustache.dev>
2024-03-14 09:29:35 -0500

    fixed up geocoding function to not pass undefined types

--- src/lib/utils/geocode.ts
+++ src/lib/utils/geocode.ts
@@ -4,23 +4,20 @@ import type { googleCalAPIType } from '$types';
 
 type geocodeOnEvent = googleCalAPIType & {
 	lnglat: LngLatLike;
-  };
+};
 
-export const geocode = async (e: googleCalAPIType | undefined):Promise<geocodeOnEvent> => {
+export const geocode = async (e: googleCalAPIType): Promise<geocodeOnEvent> => {
+	
 	const address = e?.location
-
+	if (!address?.match(/^http/) && address !== undefined){
 	const locationURL = (address: string) =>
 		`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${PUBLIC_MAPBOX}`;
 
-	if (address?.match(/^http/) || !address) {
-		return { ...e, lnglat: [-90.071533, 29.951065] }
-	} else {
 		const req = await (
 			await fetch(locationURL(address), { method: 'GET' })
 		).json();
-		return { ...e, lnglat: req.features[0].center  }
+		return { ...e, lnglat: req.features[0].center }
 	}
-
-
+	return { ...e, lnglat: [-90.071533, 29.951065] as LngLatLike }
 
 };
