50eb7abfc742afae90cdfb8f75d66713406d30ad
adonis <lacurly00@gmail.com>
2024-03-14 13:13:31 -0500

    Refactor event-card.svelte component to improve date and time formatting

--- src/lib/components/event-card.svelte
+++ src/lib/components/event-card.svelte
@@ -6,34 +6,49 @@
 	const { group, summary, start, end, location } = event;
 
 	const formatDate = (date: Date) => {
-    return date.toLocaleString('en-US', {
+    return date.toLocaleDateString('en-US', {
       month: 'short',
       day: 'numeric',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
+      year: 'numeric'
     });
   };
 
-  const startFormatted = formatDate(new Date(start));
-  const endFormatted = formatDate(new Date(end));
+  const formatTime = (date: Date) => {
+    return date.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    }).replace(' ', ''); // Remove the space before AM/PM
+  };
+
+  const startDateTime = new Date(start);
+  const endDateTime = new Date(end);
+
+  const isSameDay = startDateTime.toDateString() === endDateTime.toDateString();
+
+  const formattedDate = formatDate(startDateTime);
+  const formattedStartTime = formatTime(startDateTime);
+  const formattedEndTime = formatTime(endDateTime);
+
+  const dateTimeDisplay = isSameDay
+    ? `${formattedDate} • ${formattedStartTime} - ${formattedEndTime}`
+    : `${formatDate(startDateTime)} • ${formattedStartTime} • ${formatDate(endDateTime)} • ${formattedEndTime}`;
 </script>
 
 <article>
-	<div class="rounded-lg bg-[#E8E8E8] shadow-sm p-4 space-x-3 max-h-[200px] h-full">
-		<span>Today</span>
-		<span>Weekly</span>
+	<div class="rounded-lg bg-white dark:bg-white/10 shadow-sm p-4 space-x-3 max-h-[200px] h-full">
+		<span class="px-3 py-1 text-gray-900 font-medium text-sm rounded-md bg-gray-100 dark:bg-violet-500 dark:text-violet-100">Today</span>
+		<span class="px-3 py-1 text-gray-900 font-medium text-sm rounded-md bg-gray-100 dark:bg-violet-500 dark:text-violet-100">Weekly</span>
 	</div>
-	<div>
-		<h3>{summary}</h3>
-		<p>{group}</p>
-		<p>{startFormatted} • {endFormatted}</p>
+	<div class="p-4">
+		<h3 class="text-3xl font-bold text-gray-900 dark:text-violet-100">{summary}</h3>
+		<p class="text-lg font-semibold text-gray-600 dark:text-violet-300">{group}</p>
+		<p class="text-base text-gray-500 dark:text-violet-50">{dateTimeDisplay}</p>
 
 		{#if /^(http|https):\/\/[^ "]+$/.test(location)}
-			<a href="{location}">Virtual</a>
+			<a href="{location}" class="text-base text-gray-500 dark:text-violet-50">Virtual</a>
 		{:else}
-			<p>
+			<p class="text-base text-gray-500 dark:text-violet-50">
 				{location.split(',')[0]}
 			</p>
 		{/if}
