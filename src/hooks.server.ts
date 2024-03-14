fe6c9e644d8c1f8579dda7e32807216bcd9f585a
adonis <lacurly00@gmail.com>
2024-03-13 19:29:55 -0500

    perf(hooks): optimize database connection handling in server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
-import { start_db } from '$lib/db/db';
+import connectDB from '$lib/db/db';
 
-// starting up up the DB
-start_db()
-	.then(() => console.log('DB Started'))
-	.catch((e) => console.log(e));
+export async function handle({ event, resolve }) {
+    await connectDB(); 
+    return resolve(event);
+}
