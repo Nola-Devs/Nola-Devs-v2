023f04cbf6050c883aaa8a190d77f2e4bdf794ed
adonis <lacurly00@gmail.com>
2024-03-14 13:08:24 -0500

    Add Icon component

---
+++ src/lib/components/icon/index.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { icons, type IconName } from './icons';
+
+	export let name: IconName;
+	export let size: number = 32;
+	export let viewBox: string = '0 0 24 24';
+	export let className: string = '';
+	export let ariaHidden: boolean = false;
+
+	function getPathData(iconName: IconName) {
+		return icons[iconName] || '';
+	}
+
+	$: pathData = getPathData(name);
+</script>
+
+{#if pathData}
+	<svg
+		class={className}
+		xmlns="http://www.w3.org/2000/svg"
+		width={size}
+		height={size}
+		viewBox={viewBox}
+		fill="none"
+		aria-hidden={ariaHidden}
+	>
+		{@html pathData}
+	</svg>
+{/if}
