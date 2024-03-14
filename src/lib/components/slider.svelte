ec8e56cb1f70f604e61fa727813264b4c2e61dda
adonis <lacurly00@gmail.com>
2024-03-14 01:57:36 -0500

    Add slider component with banner and active index functionality

---
+++ src/lib/components/slider.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Banner from './banner.svelte';
+	import { writable } from 'svelte/store';
+    import type { SvelteComponent } from 'svelte';
+	import SlackIcon from '$lib/assets/icons/slackIcon.svelte';
+
+	interface BannerData {
+		title: string;
+		description: string;
+		buttonText: string;
+		onButtonClick: () => void;
+		buttonIcon?: typeof SvelteComponent | null;
+	}
+
+	let banners: BannerData[] = [
+		{
+			title: 'We connect people to local communities',
+			description:
+				'Dive into engaging discussions, share your insights, and stay updated on the latest news and events. Together, we build a thriving community! Take the plunge and explore the Nola Devs Slack channel — click below!',
+			buttonText: 'Join the Conversation',
+			onButtonClick: () => {
+				console.log('Join the Conversation');
+			},
+			buttonIcon: SlackIcon as typeof SvelteComponent,
+		},
+	];
+
+	const activeIndex = writable(0);
+
+	function setActive(index: number): void {
+		activeIndex.set(index);
+	}
+</script>
+
+<div class="relative min-h-96">
+	{#each banners as banner, index (banner.title)}
+		<div class="absolute inset-0 transition-opacity" class:hidden="{$activeIndex !== index}">
+			<Banner {...banner} />
+			{#if index === $activeIndex}
+				<div class="absolute bottom-0 left-0 right-0 flex justify-center space-x-2 p-4">
+					{#each banners as _, i (i)}
+						<button
+							class="cursor-pointer block h-2 w-2 rounded-full bg-gray-400 focus:outline-none focus:ring"
+							class:bg-white="{$activeIndex === i}"
+							aria-label="{`Go to slide ${i + 1}`}"
+							on:click="{() => setActive(i)}"
+						></button>
+					{/each}
+				</div>
+			{/if}
+		</div>
+	{/each}
+</div>
+
+<style>
+	.hidden {
+		display: none;
+	}
+</style>
