95affc26ec8de6042303e2739aeb7c775840fbaf
Jorge <jorge@codingMustache.dev>
2024-03-16 17:43:32 -0500

    added notes to add about contact and events

--- src/routes/(app)/sitemap.xml/+server.ts
+++ src/routes/(app)/sitemap.xml/+server.ts
@@ -3,19 +3,21 @@ import type { RequestHandler } from './$types';
 import GroupModel from '$lib/db/groups';
 export const GET: RequestHandler = async () => {
 	const groups: string[] = await GroupModel.distinct('group');
+	const baseAddress = 'https://www.noladevs.org/'
 
+	// TODO: Add EVENTS (prority .7), ABOUT (prority .9) AND CONTACT(prority .9) PAGES
 	return new Response(
 		`
 		<urlset>
 		<url>
-			<loc>https://www.noladevs.org/</loc>
+			<loc>${baseAddress}</loc>
 			<lastmod>${new Date().toISOString()}</lastmod>
 			<priority>1.00</priority>
 		</url>
-		${
+		${ // Groups
 			groups.map(group => (`
 				<url>
-					<loc>https://www.noladevs.org/groups/${group.replace(/ /g, '-')}</loc>
+					<loc>${baseAddress}/groups/${group.replace(/ /g, '-')}</loc>
 					<lastmod>${new Date().toISOString()}</lastmod>
 					<priority>0.80</priority>
 				</url>
