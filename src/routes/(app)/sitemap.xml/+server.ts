7ec2b4b40c186512e7c43b32a29e2e5f7961b655
Jorge <jorge@codingMustache.dev>
2024-03-16 17:32:53 -0500

    added groups and home page to sitemap

--- src/routes/(app)/sitemap.xml/+server.ts
+++ src/routes/(app)/sitemap.xml/+server.ts
@@ -1,23 +1,28 @@
 // TODO: Add sitemap generation using DB
 import type { RequestHandler } from './$types';
-
+import GroupModel from '$lib/db/groups';
 export const GET: RequestHandler = async () => {
+	const groups: string[] = await GroupModel.distinct('group');
+
 	return new Response(
 		`
-		<?xml version="1.0" encoding="UTF-8" ?>
-		<urlset
-			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-			xmlns:xhtml="https://www.w3.org/1999/xhtml"
-			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-		>
-			<!-- <url> elements go here -->
-      <loc>https://www.noladevs.dev/</loc>
-      <loc>https://www.noladevs.dev/?group=*</loc>
-      <lastmod>2023-12-02</lastmod>
-		</urlset>`.trim(),
+		<urlset>
+		<url>
+			<loc>https://www.noladevs.org/</loc>
+			<lastmod>${new Date().toISOString()}</lastmod>
+			<priority>1.00</priority>
+		</url>
+		${
+			groups.map(group => (`
+				<url>
+					<loc>https://www.noladevs.org/groups/${group.replace(/ /g, '-')}</loc>
+					<lastmod>${new Date().toISOString()}</lastmod>
+					<priority>0.80</priority>
+				</url>
+			`)).join('')
+		}
+		</urlset>
+		`,
 		{
 			headers: {
 				'Content-Type': 'application/xml'
