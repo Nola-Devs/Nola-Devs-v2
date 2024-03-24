da6c71e0e57dfd3bca8695d6de6acd8a10362680
adonis <lacurly00@gmail.com>
2024-03-24 04:05:41 -0500

    Refactor breadcrumb component to capitalize segment names

--- src/lib/components/breadcrumb.svelte
+++ src/lib/components/breadcrumb.svelte
@@ -10,11 +10,20 @@
 	const homePath = '/';
 	let pathSegments: PathSegment[] = [];
 
+	const capitalizeSegment = (segment: string) => {
+		if (segment === 'group') return 'Group';
+		return segment
+			.split('-')
+			.map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+			.join(' ');
+	};
+
 	$: if ($page) {
 		const pathArray = $page.url.pathname.split('/').filter(Boolean);
 		pathSegments = pathArray.map((segment, index): PathSegment => {
 			const href = '/' + pathArray.slice(0, index + 1).join('/');
-			return { name: segment, href };
+			const name = capitalizeSegment(segment);
+			return { name, href };
 		});
 	}
 </script>
@@ -27,7 +36,11 @@
 		>Home</BreadcrumbItem
 	>
 	{#each pathSegments as segment, index (segment.href)}
-		{#if index === pathSegments.length - 1}
+		{#if segment.name.toLowerCase() === 'group'}
+			<BreadcrumbItem>
+				{segment.name}
+			</BreadcrumbItem>
+		{:else if index === pathSegments.length - 1}
 			<BreadcrumbItem>{segment.name}</BreadcrumbItem>
 		{:else}
 			<BreadcrumbItem href="{segment.href}">{segment.name}</BreadcrumbItem>
