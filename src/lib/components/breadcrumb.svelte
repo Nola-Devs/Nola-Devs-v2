930d4b326456e1ee5a99f7563b0b03a984d1776f
adonis <lacurly00@gmail.com>
2024-03-23 21:55:08 -0500

    Refactor breadcrumb component to include additional styling and accessibility improvements

--- src/lib/components/breadcrumb.svelte
+++ src/lib/components/breadcrumb.svelte
@@ -1,32 +1,36 @@
 <script lang="ts">
-    import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
-    import { page } from '$app/stores'; 
+	import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
+	import { page } from '$app/stores';
 
-    interface PathSegment {
-      name: string;
-      href: string;
-    }
-  
-    const homePath = '/';
-    let pathSegments: PathSegment[] = [];
-  
-    $: if ($page) {
-      const pathArray = $page.url.pathname.split('/').filter(Boolean);
-      pathSegments = pathArray.map((segment, index): PathSegment => {
-        const href = '/' + pathArray.slice(0, index + 1).join('/');
-        return { name: segment, href };
-      });
-    }
-  </script>
-  
-  <Breadcrumb aria-label="Dynamic breadcrumb example">
-    <BreadcrumbItem href="{homePath}" home>Home</BreadcrumbItem>
-    {#each pathSegments as segment, index (segment.href)}
-      {#if index === pathSegments.length - 1}
-        <BreadcrumbItem>{segment.name}</BreadcrumbItem>
-      {:else}
-        <BreadcrumbItem href="{segment.href}">{segment.name}</BreadcrumbItem>
-      {/if}
-    {/each}
-  </Breadcrumb>
-  
+	interface PathSegment {
+		name: string;
+		href: string;
+	}
+
+	const homePath = '/';
+	let pathSegments: PathSegment[] = [];
+
+	$: if ($page) {
+		const pathArray = $page.url.pathname.split('/').filter(Boolean);
+		pathSegments = pathArray.map((segment, index): PathSegment => {
+			const href = '/' + pathArray.slice(0, index + 1).join('/');
+			return { name: segment, href };
+		});
+	}
+</script>
+
+<Breadcrumb aria-label="Dynamic breadcrumb" navClass="hidden lg:flex">
+	<BreadcrumbItem
+		href="{homePath}"
+		home
+		homeClass="text-gray-400 text-base font-medium inline-flex items-center dark:text-violet-100 hover:text-gray-600 dark:hover:text-violet-300"
+		>Home</BreadcrumbItem
+	>
+	{#each pathSegments as segment, index (segment.href)}
+		{#if index === pathSegments.length - 1}
+			<BreadcrumbItem>{segment.name}</BreadcrumbItem>
+		{:else}
+			<BreadcrumbItem href="{segment.href}">{segment.name}</BreadcrumbItem>
+		{/if}
+	{/each}
+</Breadcrumb>
