1f32b3697e4a08ff26d2acf6f7ecc95e8ac0301b
adonis <lacurly00@gmail.com>
2024-03-23 22:00:33 -0500

    Refactor layout server load and update layout component

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -2,6 +2,12 @@ import GroupModel from '$lib/db/groups';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async () => {
-	const groups: string[] = await GroupModel.distinct('group');
-	return { groups };
+	  const groupsData = await GroupModel.find({}, 'group slug -_id').lean();
+  
+	  const groups = groupsData.map(({ group, slug }) => ({
+		name: group,
+		slug: slug
+	  }));
+	
+	  return { groups };
 };
