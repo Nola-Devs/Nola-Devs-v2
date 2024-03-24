9da3bf7a1fd24c4f29a95921f25bd496a5d832e4
Aeshus <git@aeshus.com>
2024-03-24 15:59:27 -0500

    feat(about): create about page placeholder

---
+++ src/routes/(app)/about/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Heading, P } from 'flowbite-svelte';
+</script>
+
+<div class="flex flex-col gap-6 flex-1">
+	<Heading tag="h1">About Nola Devs</Heading>
+
+	<P
+		>We're an open-source community of passionate developers, designers, entrepreneurs, and
+		enthusiasts like you.</P
+	>
+
+	<P
+		>No matter your skill level, NOLA Devs is your place to grow. We believe in collaboration,
+		learning, and pushing the boundaries of what is possible. Here, you'll find a supportive network
+		where you can share knowledge, explore ideas, and make new friends who share your geeky
+		passions. Together, we're shaping the future of tech in New Orleans. Join us to connect, create,
+		and take your tech journey to the next level.</P
+	>
+
+	<Heading tag="h2">The NOLA Devs Team</Heading>
+
+	<Heading tag="h2">Nola Devs Contributors</Heading>
+
+	<Heading tag="h2">Community Organizers</Heading>
+</div>
