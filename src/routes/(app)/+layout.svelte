5b886133b5f11792f1c9d1f3076cb7309ef24c14
adonis <lacurly00@gmail.com>
2024-03-23 22:00:33 -0500

    Refactor layout server load and update layout component

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -4,71 +4,23 @@
 	import type { LayoutData } from './$types';
 	export let data: LayoutData;
 
-	import { NavBrand, AccordionItem, Accordion } from 'flowbite-svelte';
-	import Icon from '$lib/components/icon/index.svelte';
-	import { groupIconsMap } from '$lib/components/icon/icons';
-
-	const groups = data.groups.map((groupName) => ({
-		name: groupName,
-		iconName: groupIconsMap[groupName] || null
-	}));
-
-	const getGroupPath = (groupName: string) => `/groups/${encodeURIComponent(groupName.replace(/ /g, '-'))}`;
+	import Nav from '$lib/components/nav.svelte';
+	import Sidebar from '$lib/components/sidebar.svelte';
+	import CustomAlert, { AlertType } from '$lib/components/alert.svelte';
 </script>
 
-<div class="flex min-h-screen">
-	<aside class="bg-white dark:bg-primary-dark py-8 px-6 flex flex-col gap-12 w-full max-w-[272px] border-r border-[#EEE] dark:border-[#111827]">
-		<NavBrand
-			href="/"
-			class="inline-flex items-center justify-center font-cute text-5xl leading-10 text-gray-900 dark:text-violet-100"
-		>
-			<!-- <LogoWide styles="w-full h-12" /> -->
-			N0LA<span class="text-[#6628CC]">{'[DEVS]'}</span>
-		</NavBrand>
-
-		<div class="w-full">
-			<Accordion>
-				<!-- Events Accordion Item -->
-				<AccordionItem
-					borderOpenClass="!border-none"
-					class="py-1.5 px-2 border-none !rounded-md text-gray-900 dark:text-violet-100 hover:bg-violet-100 dark:hover:bg-violet-300/20"
-					classActive="bg-primary-light dark:bg-violet-500/20 !text-violet-500 text-base font-medium leading-[24px]"
-				>
-					<span slot="header">Events</span>
-					<p>Events here...</p>
-				</AccordionItem>
-				<!-- Tech Groups Accordion Item -->
-				<AccordionItem
-					open
-					borderOpenClass="!border-none py-1.5 px-0"
-					class="py-1.5 px-2 border-none !rounded-md text-gray-900 dark:text-violet-100 hover:bg-violet-100 dark:hover:bg-violet-300/20"
-					classActive="bg-primary-light dark:bg-violet-500/20 !text-violet-500 text-base font-medium leading-[24px]"
-				>
-					<span slot="header">Tech Groups</span>
-					<ul class="list-none p-0 space-y-1.5">
-						{#each groups as group}
-							<li class="hover:bg-purple-700 rounded-lg group transition-colors">
-								<a
-									href="{getGroupPath(group.name)}"
-									class="inline-flex items-center justify-start gap-3 py-2 px-4 text-base font-medium leading-[24px] group-hover:text-white text-gray-900 dark:text-white"
-								>
-									{#if group.iconName}
-										<Icon
-											name="{group.iconName}"
-											size="{24}"
-											className="text-gray-500 dark:text-violet-300 group-hover:text-white"
-										/>
-									{/if}
-									<span class="text-base font-medium leading-[24px]">{group.name}</span>
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</AccordionItem>
-			</Accordion>
+<div class="flex h-screen 3xl:mx-auto 3xl:max-w-screen-2xl">
+	<Sidebar {data} />
+	<main class="flex flex-1 w-full px-4 sm:px-8 3xl:px-0 md:overflow-hidden">
+		<div class="max-w-[85rem] mx-auto 3xl:px-10 flex flex-col gap-6 flex-1">
+			<Nav {data}/>
+			<CustomAlert
+				type="{AlertType.Update}"
+				message="We've updated our site, let us know how we're doing by filling a short survey"
+				link="/survey"
+				linkText="here"
+			/>
+			<slot />
 		</div>
-	</aside>
-	<main class="px-[72px] py-8 w-full">
-		<slot />
 	</main>
 </div>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,31 +1,22 @@
 <script lang="ts">
-	import type { Event } from '$types';
+	import type { Event } from '$lib/types/Event';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	let events: Event[] = data.events;
+	let events: Partial<Event[]> = data.events;
 
-	import CustomAlert, { AlertType } from '$lib/components/alert.svelte';
-	import Nav from '$lib/components/nav.svelte';
-	import Slider from '$lib/components/slider.svelte';
-	import Breadcrumb from '$lib/components/breadcrumb.svelte';
+	import Banner from '$lib/components/banner.svelte';
 	import EventList from '$lib/components/event-list.svelte';
 </script>
 
-<div class="w-full flex flex-col gap-6 h-full overflow-y-auto no-scrollbar">
-	<!-- Alert -->
-	<CustomAlert
-		type="{AlertType.Update}"
-		message="We've updated our site, let us know how we're doing by filling a short"
-		link="/survey"
-		linkText="survey"
+<div class="flex flex-col gap-6 flex-1">
+	<Banner
+		title="Developers, Designers, Lend Me Your Ears"
+		description="We’re a community of developers, entrepreneurs, students, and enthusiasts who are excited about the future of technology.  Join us to share projects, attend events, and learn something new!"
+		linkText="Community Slack"
+		linkHref="https://join.slack.com/t/nola/shared_invite/zt-256vzyuvo-1KC88G~MLCT8KCgw4QL7zg"
+		linkIcon="slackIcon"
+		bannerClass="bg-homeBanner text-white"
 	/>
-	<!-- Nav -->
-	<Nav />
-	<!-- BreadCrumb -->
-	<Breadcrumb />
-	<!-- Banner Slider -->
-	<Slider />
-	<!-- Upcomming events -->
-	<EventList {events} />
+		<EventList {events} />
 </div>
