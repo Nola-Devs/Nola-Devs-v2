3816322cfa64574d31d1ea6a99c1a4743a9c10f3
Jorge <jorge@codingMustache.dev>
2024-04-14 01:51:44 -0500

    Revert "Merge pull request #96 from Nola-Devs/94-db-service-impl"
    This reverts commit 93a6922430078aaaa5f2722ab57d494853e0bde9, reversing
    changes made to b43d9b3b35bd194579f0eee716a5286b7e1710da.

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,8 +1,13 @@
 import type { PageServerLoad } from './$types';
 import EventModel from '$lib/db/events';
 import type { Event } from '$lib/types/event.d.ts';
-import { eventService } from '$lib/services/eventService';
 
 export const load: PageServerLoad = async () => {
-	return eventService.getEvents();
+	const events = (await EventModel.find({})
+		.sort({ dateTime: 'asc' })
+		.select(['-_id', '-__v'])
+		.lean()
+		.limit(4)) as unknown as Event[];
+
+	return { events };
 };
--- src/routes/(app)/group/[slug]/events/[eventSlug]/+page.server.ts
+++ src/routes/(app)/group/[slug]/events/[eventSlug]/+page.server.ts
@@ -1,11 +1,19 @@
 import EventModel from '$lib/db/events';
 import type { PageServerLoad } from './$types';
 import type { Event } from '$lib/types/event.d.ts';
-import { eventService } from '$lib/services/eventService';
 
 export const load: PageServerLoad = async ({ params }) => {
+	const { eventSlug } = params;
+
 	try {
-		return eventService.getEventsByEventSlug(params);
+		const event: Event | null = await EventModel.findOne({ eventSlug }).select('-_id -__v').lean();
+		if (!event) {
+			throw new Error('Event not found');
+		}
+
+		// Perform any additional data transformations or fetches here
+
+		return { event };
 	} catch (error) {
 		console.error('Failed to fetch event:', error);
 		return {
