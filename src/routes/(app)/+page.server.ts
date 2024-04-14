06b1f275d8a3bf29c38af2a1a9206696ff98b99e
Jorge <jorge@codingMustache.dev>
2024-04-14 01:46:12 -0500

    calling controller

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,8 +1,16 @@
 import type { PageServerLoad } from './$types';
-import EventModel from '$lib/db/events';
 import type { Event } from '$lib/types/event.d.ts';
-import { eventService } from '$lib/services/eventService';
+import { eventController } from '$lib/db/controllers/event.controller';
+import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
-	return eventService.getEvents();
+	try {
+		const events: Event[] = await eventController.getEvents();
+
+		return {
+			events
+		};
+	} catch {
+		throw error(404, 'Group Not Found');
+	}
 };
