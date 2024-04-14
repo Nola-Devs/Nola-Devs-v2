fdbf60bbe93bc5b379ff7e7677242f4e1eeee424
Jorge <jorge@codingMustache.dev>
2024-04-14 01:45:02 -0500

    changed how calls are made

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,12 +1,13 @@
-import GroupModel from '$lib/db/groups';
+import GroupModel from '$lib/db/models/groups.model';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async () => {
-	const groupsData = await GroupModel.find({}, 'group slug -_id').lean();
+	const groupsData = await GroupModel.find({}, 'group slug icon -_id').lean();
 
-	const groups = groupsData.map(({ group, slug }) => ({
+	const groups = groupsData.map(({ group, slug, icon }) => ({
 		name: group,
-		slug: slug
+		slug,
+		icon
 	}));
 
 	return { groups };
