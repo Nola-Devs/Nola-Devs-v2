dce48a6a16f1bdd1ea3462126d343da8299a9090
Etan Joseph Heyman <etan@heyman.net>
2024-08-13 18:26:58 -0500

    Mobile screens are one scrollbar

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -10,7 +10,7 @@
 	// TODO: Setup Banner object with schema and pass into DB
 </script>
 
-<div class="flex flex-col gap-6 flex-1 h-screen">
+<div class="flex flex-col gap-6 flex-1 h-screen max-sm:h-fit">
 	<Banner
 		title="Developers, Designers, Lend Me Your Ears"
 		description="We’re a community of developers, entrepreneurs, students, and enthusiasts who are excited about the future of technology.  Join us to share projects, attend events, and learn something new!"
