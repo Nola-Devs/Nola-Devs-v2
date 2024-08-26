6040ef4ddb9e8c66540738cc0f2e2d8279efdc19
mike <sammartino.mike@gmail.com>
2024-08-26 17:06:35 -0500

    add vercel analytics inject script

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -8,6 +8,10 @@
 	import type { LayoutData } from './$types';
 	export let data: LayoutData;
 
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' })
+
 	// TODO: Add to cookies for alert if dismissed or done to not show again
 </script>
 
