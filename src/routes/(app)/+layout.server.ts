38c65eafd40361a39fd7288bf955c72e4f160a10
mike <sammartino.mike@gmail.com>
2024-08-26 20:09:10 -0500

    add inject script server side

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,6 +1,10 @@
 import GroupModel from '$lib/db/models/groups.model';
 import type { LayoutServerLoad } from './$types';
 
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+inject({ mode: dev ? 'development' : 'production' })
+
 export const load: LayoutServerLoad = async () => {
 	const groupsData = await GroupModel.find({}, 'group slug icon -_id').lean();
 
