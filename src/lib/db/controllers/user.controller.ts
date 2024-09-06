5e6533b5f7b5628630899892761974e199b0918b
mike <sammartino.mike@gmail.com>
2024-09-06 18:26:59 -0500

    add jsdoc doclets to user controller

--- src/lib/db/controllers/user.controller.ts
+++ src/lib/db/controllers/user.controller.ts
@@ -3,7 +3,16 @@ import type { User } from '$lib/types/user';
 import { error } from '@sveltejs/kit';
 
 export const userController = {
-	getAllUsersByGroup: async (group: string) => {
+
+	/**
+	 * Get all users associated with a specific group.
+	 * @async
+	 * @param {string} group Group name. See `src/lib/scripts/data/events.json` for group names.
+	 * @returns {Promise<User[]>} (fulfilled promise) Array of users associated with the group.
+	 * @throws {error} 404 if operation fails.
+	 * @example userController.getAllUsersByGroup('Hacknight')
+	 */
+	getAllUsersByGroup: async (group: string): Promise<User[]> => {
 		try {
 			const users: User[] = await UserModel.find({ group })
 				.select(['-_id', '-__v', '-role'])
@@ -13,7 +22,15 @@ export const userController = {
 			throw error(404, 'Error looking up organizers');
 		}
 	},
-	getAllOrganizers: async () => {
+
+	/**
+	 * Get all organizers from all groups.
+	 * @async
+	 * @returns {Promise<User[]>} (fulfilled promise) Array of all organizers.
+	 * @throws {error} 404 if operation fails.
+	 * @example userContoller.getAllOrganizers()
+	 */
+	getAllOrganizers: async (): Promise<User[]> => {
 		try {
 			const users: User[] = await UserModel.find({ group: { $ne: null } })
 				.select(['-_id', '-__v', '-role'])
