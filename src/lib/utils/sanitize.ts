04a62b1f8198ce9dc7c04b270aeab0eec652937f
mike <sammartino.mike@gmail.com>
2024-09-09 21:38:34 -0500

    add jsdoc to html sanitizer util

--- src/lib/utils/sanitize.ts
+++ src/lib/utils/sanitize.ts
@@ -1,5 +1,11 @@
 import sanitizeHtml from 'sanitize-html';
 
+/**
+ * Sanitize an HTML string of everything but `<p>` and `<a>`.
+ * @param {string} dirty
+ * @returns {string} An HTML string sanitized of all HTML tags other than `<p>` and `<a>`.
+ * @see https://www.npmjs.com/package/sanitize-html for other options.
+ */
 export const Sanitizer = (dirty: string) => {
 	const clean = sanitizeHtml(dirty, {
 		allowedTags: ['a', 'p'],
