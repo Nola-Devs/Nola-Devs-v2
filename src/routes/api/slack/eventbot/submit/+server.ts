bb5dc8058dbd61626764063b21316fc552900f69
Mike Sammartino <sammartino.mike@gmail.com>
2026-02-25 11:36:18 -0600

    handle 'Other Group' selection, update modal with group name input

--- src/routes/api/slack/eventbot/submit/+server.ts
+++ src/routes/api/slack/eventbot/submit/+server.ts
@@ -28,6 +28,7 @@ export const POST: RequestHandler = async ({ request }: RequestEvent) => {
 	console.log('Payload type:', payload.type);
 	if (payload.type === 'block_actions') {
 		const action = payload.actions[0];
+		console.log('action.action_id', action.action_id);
 
 		if (action.action_id === 'create_event') {
 			const groups: Group[] = await GroupModel.find({}, 'group slug');
@@ -69,39 +70,53 @@ export const POST: RequestHandler = async ({ request }: RequestEvent) => {
 			});
 		}
 
-		// if (action.action_id === 'group_select') {
-		// 	// if it's a group_select
-		// 	// we have to build the modal blocks again
-		// 	// if the other-group option is chosen, then a new field needs
-		// 	// included in between group select and others
-		// 	const selectedValue = action.selected_option.value;
-		// 	const currentGroupOptions = createGroupOptions(metadata.groups);
+		if (action.action_id === 'group_select') {
+			// if it's a group_select
+			// we have to build the modal blocks again
+			// if the other-group option is chosen, then a new field needs
+			// included in between group select and others
+			const selectedGroup = action.selected_option.value;
+			console.log('🚀 ~ POST ~ selectedGroup:', selectedGroup);
+			const groupOptions = createGroupOptions(metadata.groups);
+			const locationOptions = createLocationOptions(metadata.locations);
+			// console.log('🚀 ~ POST ~ currentGroupOptions:', groupOptions);
+			// console.log('Groups from metadata:', metadata.groups);
+			// console.log('Locations from metadata:', metadata.locations);
 
-		// 	const blocks = buildCreateEventModalBlocks();
+			const blocks = buildCreateEventModalBlocks({
+				groups: groupOptions,
+				locations: locationOptions,
+				showOtherGroupField: selectedGroup === 'other-group',
+				showOtherLocationFields: false
+			});
 
-		// 	await slackClient.views.update({
-		// 		view_id: payload.view.id,
-		// 		hash: payload.view.hash,
-		// 		view: {
-		// 			type: 'modal',
-		// 			callback_id: 'create_event_modal',
-		// 			// private_metadata: JSON.stringify(updatedMetadata),
-		// 			title: {
-		// 				type: 'plain_text',
-		// 				text: 'Create an Event'
-		// 			},
-		// 			submit: {
-		// 				type: 'plain_text',
-		// 				text: 'Submit'
-		// 			},
-		// 			close: {
-		// 				type: 'plain_text',
-		// 				text: 'Cancel'
-		// 			},
-		// 			blocks
-		// 		}
-		// 	});
-		// }
+			await slackClient.views.update({
+				view_id: payload.view.id,
+				hash: payload.view.hash,
+				view: {
+					type: 'modal',
+					callback_id: 'create_event_modal',
+					// private_metadata: JSON.stringify(updatedMetadata),
+					title: {
+						type: 'plain_text',
+						text: 'Create an Event'
+					},
+					submit: {
+						type: 'plain_text',
+						text: 'Submit'
+					},
+					close: {
+						type: 'plain_text',
+						text: 'Cancel'
+					},
+					blocks
+				}
+			});
+		}
+
+		if (action.action_id === 'location_select') {
+			// then do this
+		}
 
 		if (action.action_id === 'edit_event') {
 			// then do this
