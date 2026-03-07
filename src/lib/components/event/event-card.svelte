17b5577d0a2d8c96b23ee85aace8a13e63597f85
Mike Sammartino <sammartino.mike@gmail.com>
2026-03-07 14:06:40 -0600

    fix location.split type error

--- src/lib/components/event/event-card.svelte
+++ src/lib/components/event/event-card.svelte
@@ -78,29 +78,14 @@
 		<div class="w-full md:w-1/2">
 			<h3 class="text-sm text-balance md:text-xl font-semibold text-[#333] dark:text-violet-100">
 				{meetupName}
-				<!-- <span class="inline-flex ml-2">
-					<Icon
-						name="recurrenceIcon"
-						className="w-[16px] h-[16px] md:w-[18px] md:h-[18px]"
-						viewBox="0 0 18 18"
-					/>
-				</span> -->
 			</h3>
 			<p class="text-sm md:text-base font-medium text-[#4F4F4F] dark:text-violet-300">
 				{groupName}
 			</p>
 
-			{#if /^(http|https):\/\/[^ "]+$/.test(location)}
-				<a
-					href="{location}"
-					class="text-sm md:text-base text-gray-500 dark:text-violet-50 hover:underline"
-					>Virtual Event</a
-				>
-			{:else}
-				<p class="text-sm md:text-base text-gray-500 dark:text-violet-50">
-					{location.name}
-				</p>
-			{/if}
+			<p class="text-sm md:text-base text-gray-500 dark:text-violet-50">
+				{location.name}
+			</p>
 		</div>
 	</div>
 
